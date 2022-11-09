0d724b6e18368484c18e95fb5d3d71bdd934f61a
Alisha <alishamohasin85@gmail.com>
2022-11-09 23:57:39 +0530

    updated cart

--- Frontend/src/Router/AllRoutes.jsx
+++ Frontend/src/Router/AllRoutes.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { Cart } from "../Components/Cart/Cart";
 import Home from "../Components/Home";
+import SingleProduct from "../Components/SingleProduct/SingleProduct";
 
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/Cart" element={<Cart />} />
+      <Route path="/Singleproduct" element={<SingleProduct/>}/>
     </Routes>
   );
 };
