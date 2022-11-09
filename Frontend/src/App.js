de12f650bae887fc0201976785681fc7c5712f0c
Abhishek Pratap Solanki <abhisheksolanki1020@gmail.com>
2022-11-09 22:48:15 +0530

    Frontend Added

---
+++ Frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AllRoutes from './Router/AllRoutes';
+
+function App() {
+  return (
+    <div className="App">
+    <AllRoutes />
+    </div>
+  );
+}
+
+export default App;
