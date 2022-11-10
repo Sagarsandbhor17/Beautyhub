288bb922aeb27fcd77ede9ce2bf0884c341ff4d2
sombir5 <sombirverma5@gmail.com>
2022-11-10 21:28:16 +0530

    Landing page

--- Frontend/src/Components/Landing/LandingPage.jsx
+++ Frontend/src/Components/Landing/LandingPage.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import Carousel from "./Carousel"
-import Middle from './Middle'
+import React from "react";
+import Footer from "../Footer/Footer";
+import Carousel from "./Carousel";
+import Middle from "./Middle";
 const LandingPage = () => {
   return (
-      <div>
-          <Carousel />
-          <Middle/>
-   </div>
-  )
-}
+    <div>
+      <Carousel />
+      <Middle />
+      <Footer/>
+    </div>
+  );
+};
 
-export default LandingPage
+export default LandingPage;
--- Frontend/src/Router/AllRoutes.jsx
+++ Frontend/src/Router/AllRoutes.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../Components/Home";
 import LandingPage from "../Components/Landing/LandingPage";
+import Skin from "../Components/Skin/Skin"
 import Login from "../Components/Login-Signup/Login";
 import Signup from "../Components/Login-Signup/Signup";
 
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/landing" element={<LandingPage />} />
+      {/* <Route path="/" element={<Home />} /> */}
+      <Route path="/" element={<LandingPage />} />
+      {/* <Route path="/" element={<Skin />} /> */}
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
     </Routes>
