0c42aea13393b627b8360ef393791183b0ae28c1
Alisha <alishamohasin85@gmail.com>
2022-11-11 22:05:26 +0530

    updated

--- Frontend/src/Router/AllRoutes.jsx
+++ Frontend/src/Router/AllRoutes.jsx
@@ -4,28 +4,33 @@ import { Cart } from "../Components/Cart/Cart";
 import Checkout from "../Components/Cart/Checkout";
 import OrderConfirm from "../Components/Cart/OrderConfirm";
 import Otp from "../Components/Cart/Otp";
-import Home from "../Components/Home";
 import LandingPage from "../Components/Landing/LandingPage";
 import Skin from "../Components/Skin/Skin"
 import Login from "../Components/Login-Signup/Login";
 import Signup from "../Components/Login-Signup/Signup";
-import DummyProduct from "../Components/SingleProduct/DummyProduct";
 import SingleProduct from "../Components/SingleProduct/SingleProduct";
+// import Navbar from "../Components/Navbar/Navbar";
+import Authprovider from "../Components/Private/Authprovider";
 
 const AllRoutes = () => {
   return (
     <Routes>
 
+      {/* <Route path="/" element={<Navbar/>}/> */}
+      <Route path="/" element={<LandingPage />} />
       <Route path="/skin" element={<Skin />} />
-      <Route path="/" element={<LandingPage/> }/>
       <Route path="/login" element={<Login />}/>
       <Route path="/signup" element={<Signup />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/products/:id" element={<SingleProduct />} />
+      <Route path="/cart" element={
+      <Authprovider>
+      <Cart />
+      </Authprovider>
+      } />
+      <Route path="/singleproduct" element={<SingleProduct />} />
       <Route path="/checkout" element={<Checkout />} />
       <Route path="/otp" element={<Otp />} />
-      <Route path="/orderplaced" element={<OrderConfirm />} />
-      {/* <Route path="/dummyproduct" element={ <DummyProduct/>}/> */}
+      <Route path="/orderplaced" element={<OrderConfirm/>}/>
+
 
     </Routes>
   );
