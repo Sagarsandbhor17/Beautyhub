68364e560224a93426227e9cfbdbecc46fd42335
Mayur-Pc <sagar12500@gmail.com>
2022-11-14 12:49:12 +0530

    cart-model-updated

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const connect = () =>{
-    return mongoose.connect("mongodb+srv://beauty:hub@cluster0.gjxxf5s.mongodb.net/beautyhub")
+    return mongoose.connect(process.env.MONGOURL);
 };
 
 module.exports = connect;
